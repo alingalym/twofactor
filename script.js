33a49673fa2fc9de3a5fe9fa2bb8b1b887372c39
alin galym <alin_galym@hotmail.com>
2023-05-01 19:20:08 +0600

    4 update

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
 // Подключаемся к контракту
-const contractAddress = "0xf6046beAC626977C66349f3Bfc9A616B4AE8320E"; //Address of contract
+const contractAddress = "0x9936a7635af7d00c4Cf955Caa725eA155fA82b19"; //Address of contract
 
 // Указываем ABI (Application Binary Interface) контракта
 const abi = [
 	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "acc",
+				"type": "string"
+			}
+		],
+		"name": "Register",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "acc",
+				"type": "string"
+			}
+		],
+		"name": "UnRegister",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "accounts",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "addr",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [
@@ -41,19 +103,13 @@ const abi = [
 		"type": "function"
 	},
 	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "acc",
-				"type": "string"
-			}
-		],
-		"name": "Login",
+		"inputs": [],
+		"name": "Length",
 		"outputs": [
 			{
-				"internalType": "bool",
+				"internalType": "uint256",
 				"name": "",
-				"type": "bool"
+				"type": "uint256"
 			}
 		],
 		"stateMutability": "view",
@@ -67,55 +123,12 @@ const abi = [
 				"type": "string"
 			}
 		],
-		"name": "Register",
+		"name": "Login",
 		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "acc",
-				"type": "string"
-			}
-		],
-		"name": "UnRegister",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
 			{
 				"internalType": "uint256",
 				"name": "",
 				"type": "uint256"
-			}
-		],
-		"name": "accounts",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "addr",
-				"type": "address"
 			}
 		],
 		"stateMutability": "view",
@@ -142,10 +155,10 @@ const provider = new ethers.providers.Web3Provider(window.ethereum, 97);
 let signer;
 let contract;
 const acc = [];
-acc[0] = "user";
+acc.push("user");
 const pass = [];
-pass[0]= "password";
-let length = 0;
+pass.push("password");
+let length = 1;
 
 //Запрашиваем аккаунты пользователя и подключаемся к первому аккаунту
 provider.send("eth_requestAccounts", []).then(() => {
@@ -158,29 +171,33 @@ provider.send("eth_requestAccounts", []).then(() => {
 });
 
 function check_A(usr) {
-	for (let i=0; i<=length; i++) {
-		if (acc[i]==usr) return true;
+	for (let i=0; i<length; i++) {
+		if (acc[i]==usr) return i;
 	}
-	return false;
+	return 99999;
 }
 
 function check_AA(usr, pswd) {
-	for (let i=0; i<=length; i++) {
-		if ((acc[i]==usr) && (pass[i]==pswd)) return true;
+	for (let i=0; i<length; i++) {
+		if ((acc[i]==usr) && (pass[i]==pswd)) return i;
 	}
-	return false;
+	return 99999;
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 //Вызываем Login() в смарт-контракте и показываем результаты
 async function Login() {
   	const user = document.getElementById("user").value;
   	const password = document.getElementById("password").value;
-  	if (check_A(user)==true) {
-  		if (check_AA(user,password)==true) {
-			const getTBNB = await contract.Login(user); 
+  	if (check_A(user)!=99999) {
+  		if (check_AA(user,password)!=99999) {
+			let getTBNB = await contract.Login(user); 
 			//wait for contract execution
-			//getTBNB.wait(1);
-			if (getTBNB == true) {
+			//getTBNB.wait(); Do we need to create an event inside of contract?
+			if (getTBNB == 1) {
 				document.getElementById("result").innerText = user;
 				document.getElementById("info").innerText = "Login is succesful!";
 			}
@@ -204,50 +221,65 @@ async function Login() {
 async function Register() {
 	const user = document.getElementById("user").value;
 	const password = document.getElementById("password").value;
-  	if (check_A(user)==true) {
+	//let setTBNB = 0;
+  	if (check_A(user)!=99999) {
 		document.getElementById("result").innerText = user;
 		//document.getElementById("info").innerText = "This account is already existing!";
+		//sleep(6000);
 		const setTBNB = await contract.Register(user);
-		if (setTBNB == true){
+		
+		//if (setTBNB != 99999){
 			document.getElementById("result").innerText = user;
-			document.getElementById("info").innerText = "TBNB Account is added!";
-		}
+			document.getElementById("info").innerText = "TBNB Account is added, please try to logon upon Metamask confirmation.";
+		/*}
 		else {
 			document.getElementById("result").innerText = user;
-			document.getElementById("info").innerText = "Account is not added: TBNB address is existing!";
-		}
+			document.getElementById("info").innerText = "Account is not added:: TBNB address is existing!";
+		}*/
   	}
   	else{
 		const setTBNB = await contract.Register(user); 
 		//wait for contract execution
-		//setTBNB.wait(1);
-		if (setTBNB == true){
-			acc[length]=user;
-			pass[length]=password;
+		//sleep(6000);
+		//if (getTBNB != 99999){
+			acc.push(user);
+			pass.push(password);
 			length=length+1;
   			document.getElementById("result").innerText = user;
-			document.getElementById("info").innerText = "Account is added: the total number is "+length;
-		}
+			document.getElementById("info").innerText = "Account is added: the total number is "+length+" Please try to logon upon Metamask completion.";
+		/*}
 		else {
 			document.getElementById("result").innerText = user;
 			document.getElementById("info").innerText = "Account is not added: TBNB address is existing!";
-		}
+		}*/
   	}
 }
 
-
 async function UnRegister() {
 	const user = document.getElementById("user").value;
+	let i = check_A(user);
+	let k = length-1;
 	//const password = document.getElementById("password").value;
-	if (check_A(user)!=true) {
+	if (i==99999) {
 		document.getElementById("result").innerText = user;
 		document.getElementById("info").innerText = "This account is not existing!";
 	}
 	else{
-		const delTBNB = await contract.UnRegister(user); 
+		await contract.UnRegister(user); 
 		//wait for contract execution
 		//delTBNB.wait(1);
-		if (delTBNB == true){
+		let getTBNB = await contract.Login(user);
+		if (getTBNB == 1){
+			if (i != k) { //swap i and last record
+				let acc_temp = acc[i];
+				let pass_temp = pass[i];
+				acc[i] = acc[k];
+				pass[i] = pass[k];
+				acc[k] = acc_temp;
+				pass[k] = pass_temp;
+			}
+			acc.pop();
+			pass.pop();
 			length=length-1;
 			document.getElementById("result").innerText = user;
 			document.getElementById("info").innerText = "Account was removed: the total number is "+length;
